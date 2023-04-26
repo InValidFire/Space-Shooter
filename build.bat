a30d8d866bbb3277b0b85aebd6d01fb332b1a4c7
Riley Housden <riley.t.housden@gmail.com>
2023-04-26 01:14:45 -0400

    update build script

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
 @echo off
 echo Starting build script
-nuitka --standalone --onefile --include-package=game_lib --include-data-dir=assets -o "rogers_revenge.exe" main.py
+pyxel package . main.py
+pyxel app2html rogers_revenge.pyxapp
+move rogers_revenge.pyxapp dist
+move rogers_revenge.html dist
+nuitka --standalone --onefile --include-package=game_lib --include-data-dir=assets=assets --output-filename="dist/rogers_revenge.exe" main.py
