8eab678d1f2486aab2a253f27e496319498d1496
Riley Housden <riley.t.housden@gmail.com>
2023-04-26 08:32:58 -0400

    change build files directory

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @echo off
 echo Starting build script
+rmdir build /s /q
 pyxel package . main.py
 pyxel app2html rogers_revenge.pyxapp
 move rogers_revenge.pyxapp dist
 move rogers_revenge.html dist
-nuitka --standalone --onefile --include-package=game_lib --include-data-dir=assets=assets --output-filename="dist/rogers_revenge.exe" main.py
+nuitka --standalone --onefile --include-package=game_lib --windows-disable-console --include-data-dir=assets=assets --output-dir=build --output-filename="dist/rogers_revenge.exe" main.py
