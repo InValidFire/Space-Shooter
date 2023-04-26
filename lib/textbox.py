ff985f49b3433a6f63d262e82c09ddf954d7e1f6
Riley Housden <riley.t.housden@gmail.com>
2023-04-25 23:42:48 -0400

    Add TextBox class

---
+++ lib/textbox.py
@@ -0,0 +1,46 @@
+import pyxel as px
+from . import GameObject, Game, Box, Vector
+
+CENTERED = 0
+RIGHT = 1
+LEFT = -1
+TOP = -1
+BOTTOM = 1
+
+
+class TextBox(GameObject):
+    def __init__(self, game: Game, text: str, color: int) -> None:
+        super().__init__(game)
+        self.text = text
+        self.color = color
+        size = self.calculate_size()
+        self.shape = Box(size[0], size[1])
+
+    def place(self, horizontal_pos: int, vertical_pos: int):
+        if horizontal_pos == -1:
+            horizontal_pos = 2
+        elif horizontal_pos == 0:
+            horizontal_pos = (self.game.screen.width - self.shape.width)/2
+        elif horizontal_pos == 1:
+            horizontal_pos = self.game.screen.width - self.shape.width
+
+        if vertical_pos == -1:
+            vertical_pos = 2
+        elif vertical_pos == 0:
+            vertical_pos = (self.game.screen.height - self.shape.height)/2
+        elif vertical_pos == 1:
+            vertical_pos = self.game.screen.height - self.shape.height
+        self.pos = Vector(horizontal_pos, vertical_pos)
+
+    def calculate_size(self):
+        length = 0
+        width = 0
+        rows = self.text.split()
+        for row in rows:
+            width += 5
+            for char in row:
+                length += 4
+        return length, width
+
+    def draw(self):
+        px.text(self.pos.x, self.pos.y, self.text, self.color)
